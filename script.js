85e67dec7f5e5eadf1e2deeff1ff4f420d79e17c
YgorB <92278272+YgorB@users.noreply.github.com>
2022-12-15 23:13:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let List = document.getElementById('List');
+let Add = document.getElementById('Add');
+let result = document.getElementById('result');
+let Putlist = document.getElementById('PutList');
+
+
+
